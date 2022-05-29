f88dc56c359279459be04d0faa22b156ae3fbef6
Åke Engelbrektson <eson57@users.noreply.github.com>
2022-05-29 09:16:35 +0200

    Create harbour-selfossc-sv.ts
    Add Swedish translation, please.

---
+++ translations/harbour-selfossc-sv.ts
@@ -0,0 +1,263 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Unread</source>
+        <translation>Oläst</translation>
+    </message>
+</context>
+<context>
+    <name>DetailsPage</name>
+    <message>
+        <source>Mark as unread</source>
+        <translation>Markera som oläst</translation>
+    </message>
+    <message>
+        <source>Mark as read</source>
+        <translation>Markera som läst</translation>
+    </message>
+    <message>
+        <source>Star</source>
+        <translation>Stjärnmärk</translation>
+    </message>
+    <message>
+        <source>Unstar</source>
+        <translation>Ta bort stjärna</translation>
+    </message>
+    <message>
+        <source>Open in Browser</source>
+        <translation>Öppna i webbläsaren</translation>
+    </message>
+    <message>
+        <source>via </source>
+        <translation>via </translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Okänd</translation>
+    </message>
+</context>
+<context>
+    <name>FilterDialog</name>
+    <message>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <source>Newest</source>
+        <translation>Nyast</translation>
+    </message>
+    <message>
+        <source>Unread</source>
+        <translation>Oläst</translation>
+    </message>
+    <message>
+        <source>Starred</source>
+        <translation>Stjärnmärkt</translation>
+    </message>
+    <message>
+        <source>Filter</source>
+        <translation>Filter</translation>
+    </message>
+    <message>
+        <source>All</source>
+        <translation>Alla</translation>
+    </message>
+    <message>
+        <source>Source</source>
+        <translation>Källa</translation>
+    </message>
+    <message>
+        <source>Tag</source>
+        <translation>Tagg</translation>
+    </message>
+</context>
+<context>
+    <name>ListItemComponent</name>
+    <message>
+        <source>Star</source>
+        <translation>Stjärnmärk</translation>
+    </message>
+    <message>
+        <source>Unstar</source>
+        <translation>Ta bort stjärna</translation>
+    </message>
+    <message>
+        <source>Mark as unread</source>
+        <translation>Markera som oläst</translation>
+    </message>
+    <message>
+        <source>Open in Browser</source>
+        <translation>Öppna i webbläsaren</translation>
+    </message>
+    <message>
+        <source>via </source>
+        <translation>via </translation>
+    </message>
+    <message>
+        <source>Mark above as read</source>
+        <translation>Markera ovanstående som läst</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Okänd</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <source>Mark all as read</source>
+        <translation>Markera alla som lästa</translation>
+    </message>
+    <message>
+        <source>Top</source>
+        <translation>Topp</translation>
+    </message>
+    <message>
+        <source>Unread</source>
+        <translation>Oläst</translation>
+    </message>
+    <message>
+        <source>Newest</source>
+        <translation>Nyast</translation>
+    </message>
+    <message>
+        <source>Starred</source>
+        <translation>Stjärnmärkt</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Okänd</translation>
+    </message>
+    <message>
+        <source>Show thumbnails</source>
+        <translation>Visa miniatyrer</translation>
+    </message>
+    <message>
+        <source>Hide thumbnails</source>
+        <translation>Dölj miniatyrer</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Server</source>
+        <translation>Server</translation>
+    </message>
+    <message>
+        <source>Server url</source>
+        <translation>Serveradress</translation>
+    </message>
+    <message>
+        <source>Enable debugging</source>
+        <translation>Aktivera felsökning</translation>
+    </message>
+    <message>
+        <source>Username (optional)</source>
+        <translation>Användarnamn (tillval)</translation>
+    </message>
+    <message>
+        <source>Password (optional)</source>
+        <translation>Lösenord (tillval)</translation>
+    </message>
+    <message>
+        <source>Server timezone (optional)</source>
+        <translation>Servertidszon (tillval)</translation>
+    </message>
+    <message>
+        <source>Behaviour</source>
+        <translation>Beteende</translation>
+    </message>
+    <message>
+        <source>Always show thumbnails</source>
+        <translation>Visa alltid miniatyrer</translation>
+    </message>
+    <message>
+        <source>Scale images in content</source>
+        <translation>Skala bilder i innehåll</translation>
+    </message>
+    <message>
+        <source>WebView</source>
+        <translation>Webbvy</translation>
+    </message>
+    <message>
+        <source>Font size</source>
+        <translation>Teckenstorlek</translation>
+    </message>
+    <message>
+        <source>Font color</source>
+        <translation>Teckenfärg</translation>
+    </message>
+    <message>
+        <source>Background color</source>
+        <translation>Bakgrundsfärg</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <source>Clear cache</source>
+        <translation>Rensa cachelagring</translation>
+    </message>
+</context>
+<context>
+    <name>WebViewPage</name>
+    <message>
+        <source>Stop</source>
+        <translation>Stopp</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>Läs in igen</translation>
+    </message>
+    <message>
+        <source>Open in Browser</source>
+        <translation>Öppna i webbläsaren</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>Tillbaka</translation>
+    </message>
+    <message>
+        <source>Reload to view original web page</source>
+        <translation>Läs in igen för att visa ursprunglig webbsida</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-selfossc</name>
+    <message>
+        <source>All</source>
+        <translation>Alla</translation>
+    </message>
+</context>
+<context>
+    <name>selfoss</name>
+    <message>
+        <source>just now</source>
+        <translation>just nu</translation>
+    </message>
+    <message>
+        <source> seconds ago</source>
+        <translation> sekunder sedan</translation>
+    </message>
+    <message>
+        <source> minutes ago</source>
+        <translation> minuter sedan</translation>
+    </message>
+</context>
+</TS>
